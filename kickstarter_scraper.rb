f9893087b690ee0327374e405034431ce45a5a7c
Jeremy Wood <js.wood206@gmail.com>
2020-09-23 03:49:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,4 +8,15 @@ def create_project_hash
   binding.pry
 end
 
+projects: 
+  kickstarter.css("li.project.grid_4").first
+title:
+  project.css("h2.bbcard_name strong a").text
+image link: 
+  project.css("div.project-thumbnail a img").attribute("src").value
+description: 
+  project.css("p.bbcard_blurb").text
+location:
+  project.css("ul.project-meta").text
+
 create_project_hash
