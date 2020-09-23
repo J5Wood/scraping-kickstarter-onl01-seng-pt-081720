9e4131201a6affdeba7dafbfdf084af681e99440
Jeremy Wood <js.wood206@gmail.com>
2020-09-23 03:51:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,6 +17,6 @@ image link:
 description:
   project.css("p.bbcard_blurb").text
 location:
-  project.css("ul.project-meta").text
+  project.css("ul.project-meta span.location-name").text
 
 create_project_hash
